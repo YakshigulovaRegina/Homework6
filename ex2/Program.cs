2456861f3037ac7b9f5fc269afe72cf7b70f64a5
Regina <gumerovars@gmail.com>
2023-01-16 20:44:44 +0500

    second task

---
+++ ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿double getX(double k1, double k2, double b1, double b2)
+{
+    return (b2 - b1)/(k1 - k2); 
+}
+
+double getY(double k1, double k2, double b1, double b2)
+{
+    return k1 * ((b2 - b1)/(k1 - k2)) + b1; 
+}
+
+Console.WriteLine("Введите точку b1 ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите точку k1 ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите точку b2 ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите точку k2 ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = getX(k1, k2, b1, b2);
+double y = getY(k1, k2, b1, b2);
+Console.WriteLine("(" + x + ";" + y + ")");
