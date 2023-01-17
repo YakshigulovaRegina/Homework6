e07c1281cf6e0d6d89a75d8c5713d77901f4932b
Regina <gumerovars@gmail.com>
2023-01-17 14:35:08 +0500

    add condition

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -18,4 +18,12 @@ Console.WriteLine("Введите точку k2 ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 double x = getX(k1, k2, b1, b2);
 double y = getY(k1, k2, b1, b2);
-Console.WriteLine("(" + x + ";" + y + ")");
+
+
+if ((k1 == k2) && (b1 == b2))
+{ Console.WriteLine("Прямые совпадают "); }
+else if (k1 == k2)
+{ Console.WriteLine("Прямые параллельны ");}
+else {Console.WriteLine("(" + x + ";" + y + ")");}
+
+
