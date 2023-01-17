6f0ab5c46ca2b3c2d2a6b3b49c1e1fe8c547b4d9
Regina <gumerovars@gmail.com>
2023-01-17 14:54:34 +0500

    update ex2

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -16,14 +16,16 @@ Console.WriteLine("Введите точку b2 ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите точку k2 ");
 double k2 = Convert.ToDouble(Console.ReadLine());
-double x = getX(k1, k2, b1, b2);
-double y = getY(k1, k2, b1, b2);
+
 
 
 if ((k1 == k2) && (b1 == b2))
 { Console.WriteLine("Прямые совпадают "); }
 else if (k1 == k2)
 { Console.WriteLine("Прямые параллельны ");}
-else {Console.WriteLine("(" + x + ";" + y + ")");}
+else {
+double x = getX(k1, k2, b1, b2);
+double y = getY(k1, k2, b1, b2);
+Console.WriteLine("(" + x + ";" + y + ")");}
 
 
